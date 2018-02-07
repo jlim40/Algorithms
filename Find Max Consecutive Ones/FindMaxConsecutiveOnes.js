67d95d285fa1d0812d09dae7d7390ab53c5fbc6f
Jihoon Lim <jihoon2514@gmail.com>
2018-02-06 20:17:48 -0500

    2018/02/06 Algorithm Study

--- Find Max Consecutive Ones/FindMaxConsecutiveOnes.js
+++ Find Max Consecutive Ones/FindMaxConsecutiveOnes.js
@@ -11,7 +11,7 @@ Note:
 The input array will only contain 0 and 1.
 The length of input array is a positive integer and will not exceed 10,000
 */
-
+ 
 /**
  * @param {number[]} nums
  * @return {number}
