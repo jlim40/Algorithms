7dbc7803795227d9ad49fa8ff7a2d28333b52583
Jihoon Lim <jihoon2514@gmail.com>
2018-02-17 10:23:25 -0500

    2018/02/17 Algorithm Study

---
+++ Intersection of Two Arrays/IntersectionOfTwoArrays.js
@@ -0,0 +1,32 @@
+/*
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Note:
+Each element in the result must be unique.
+The result can be in any order.
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+    let intersected = set1.intersection(set2);
+    return [...intersected];
+}
+
+Set.prototype.intersection = function(setB) {
+    var intersection = new Set();
+    for (var elem of setB) {
+        if (this.has(elem)) {
+            intersection.add(elem);
+        }
+    }
+    return intersection;
+}
