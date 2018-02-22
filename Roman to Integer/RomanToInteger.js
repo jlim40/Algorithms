122b27718fe8965eac569551e57478546e74cea6
Jihoon Lim <jihoon2514@gmail.com>
2018-02-21 20:30:04 -0500

    2018/02/20 Algorithm Study

---
+++ Roman to Integer/RomanToInteger.js
@@ -0,0 +1,34 @@
+/*
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+let getIntFromRoman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+};
+
+var romanToInt = function(s) {
+    let sum = 0;
+    let curr, next;
+    for(let i = 0; i < s.length-1; i++) {
+        curr = getIntFromRoman[s[i]];
+        next = getIntFromRoman[s[i+1]];
+        if(curr < next)
+            sum -= curr;
+        else
+            sum += curr;
+    }
+    return sum + getIntFromRoman[s[s.length-1]];
+};
