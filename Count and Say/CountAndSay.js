9dece7615443698e4d7a4ef203bbb28fdb645082
Jason Lim <jihoon2514@gmail.com>
2018-03-31 10:56:51 -0400

    2018/03/31 Algorithm Study

---
+++ Count and Say/CountAndSay.js
@@ -0,0 +1,55 @@
+/*
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth term of the count-and-say sequence.
+
+Note: Each term of the sequence of integers will be represented as a string.
+
+Example 1:
+
+Input: 1
+Output: "1"
+Example 2:
+
+Input: 4
+Output: "1211"
+*/
+
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+  let countIdx = s => {
+    let c = s[0];
+    let str = '';
+    let count = 1;
+    let i = 1;
+    for (; i < s.length; ++i) {
+      if (s[i] === c) {
+        ++count;
+      } else {
+        str = str + count + c;
+        count = 1;
+        c = s[i];
+      }
+    }
+    str = str + count + c;
+    return str;
+  };
+
+  let str = '1';
+  for (let i = 1; i < n; ++i) {
+    str = countIdx(str);
+  }
+
+  return str;
+};
